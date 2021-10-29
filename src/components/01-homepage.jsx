6a235fdffa19973fbb1d0f18d7515f7d64e91ae2
marcosaccarola <marco.sacca.ads@gmail.com>
2021-10-29 15:05:30 +0200

    fetching position data by query

--- src/components/01-homepage.jsx
+++ src/components/01-homepage.jsx
@@ -2,24 +2,33 @@ import { useState } from "react"
 import { Col, Container, Form, FormControl, Row } from "react-bootstrap"
 
 
-const HomePage=()=>{
-    const[position,setPosition]=useState('')
-    const[positionData,setPositionData]=useState()
 
-    // const fetchPositionData=(position)=>{
-    //     try {
-    //         let response=await fetch()
-    //         if(response.ok){
-    //             let data=await response.json()
-    //             let 
-    //             setOnlineUsers(onlineUsers)
-    //         }else{
-    //             console.log('SOMETHING WENT WRONG')
-    //         }
-    //     } catch (error) {
-    //         console.log(error)
-    //     }
-    // }
+const HomePage=()=>{
+    const[query,setQuery]=useState('')
+    const[positionData,setPositionData]=useState([])
+
+    //************************************** POSITION DATA ***************************************
+    const handleChange=(e)=>{setQuery(e.target.value)}
+    const handleSubmit=(e)=>{
+        e.preventDefault()
+        getPositionData(query)
+    }
+    const APP_ID=process.env.REACT_APP_APP_ID
+    const URL='http://api.openweathermap.org/data/2.5/weather?q='
+    const getPositionData=async(query)=>{
+        try {
+            let response=await fetch(URL+query+APP_ID)
+            if(response.ok){
+                let data=await response.json()
+                setPositionData(data)
+            }else{
+                console.log('SOMETHING WENT WRONG')
+            }
+        } catch (error) {
+            console.log(error)
+        }
+        console.log('POSITION DATA FROM FETCH',positionData)
+    }
 
     return(
         <Container fluid className="px-4">
@@ -30,13 +39,12 @@ const HomePage=()=>{
             </Row>
             <Row>
                 <Col>
-                    <Form 
-                    // </Col>onSubmit={fetchPositionData}
-                    >
+                    <Form onSubmit={handleSubmit}>
                         <FormControl 
-                            placeholder='text a place'
-                            value={position}
-                            onChange={(e)=>setPosition(e.target.value)}
+                            type="search"
+                            value={query}
+                            onChange={(e)=>handleChange(e)}
+                            placeholder="text a cityname"
                         />
                     </Form>
                 </Col>
