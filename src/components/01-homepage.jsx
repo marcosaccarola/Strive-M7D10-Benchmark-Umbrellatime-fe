b898e9eb1dd31230b14eb8fce18832aa08025de3
marcosaccarola <marco.sacca.ads@gmail.com>
2021-10-29 23:59:15 +0200

    external fetches (in utils)

--- src/components/01-homepage.jsx
+++ src/components/01-homepage.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import { Col, Container, Form, FormControl, ListGroup, Row } from "react-bootstrap"
+import { getActualData, getForecastData } from "../utils/fetch"
 import RealTimeWeather from "./01a-RealTimeWeather"
 import ForecastWeather from "./01b-ForecastWeather"
 
@@ -7,45 +8,14 @@ const HomePage=()=>{
     const[query,setQuery]=useState('')
     const[actualData,setActualData]=useState()
     const[forecastData,setForecastData]=useState()
-    const APP_ID=process.env.REACT_APP_APP_ID
+    
     const handleChange=(e)=>{setQuery(e.target.value)}
     const handleSubmit=(e)=>{
         e.preventDefault()
-        getActualData(query)
-        getForecastData(query)
+        getActualData(query,setActualData)
+        getForecastData(query,setForecastData)
     }
-    //************************************** ACTUAL DATA BY POSITION ***************************************
-    const URL='http://api.openweathermap.org/data/2.5/weather?q='
-    const getActualData=async(query)=>{
-        try {
-            let response=await fetch(URL+query+APP_ID)
-            if(response.ok){
-                let data=await response.json()
-                setActualData(data)
-                console.log('ACTUAL DATA FROM FETCH',data)
-            }else{
-                console.log('SOMETHING WENT WRONG')
-            }
-        } catch (error) {
-            console.log(error)
-        }
-    }
-    //********************************* FORECAST DATA BY POSITION *******************************************
-    const URL_FORECAST='http://api.openweathermap.org/data/2.5/forecast?q='
-    const getForecastData=async(query)=>{
-        try {
-            let response=await fetch(URL_FORECAST+query+APP_ID)
-            if(response.ok){
-                let data=await response.json()
-                setForecastData(data)
-                console.log('FORECAST DATA FROM FETCH',data)
-            }else{
-                console.log('SOMETHING WENT WRONG')
-            }
-        } catch (error) {
-            console.log(error)
-        }
-    }
+
 
     return(
         <Container fluid className="w-50 mt-5">
---
+++ src/utils/fetch.js
@@ -0,0 +1,33 @@
+const APP_ID=process.env.REACT_APP_APP_ID
+//************************************** ACTUAL DATA BY POSITION ***************************************
+const URL='http://api.openweathermap.org/data/2.5/weather?q='
+export const getActualData=async(query,setActualData)=>{
+    try {
+        let response=await fetch(URL+query+APP_ID)
+        if(response.ok){
+            let data=await response.json()
+            setActualData(data)
+            console.log('ACTUAL DATA FROM FETCH',data)
+        }else{
+            console.log('SOMETHING WENT WRONG')
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+//********************************* FORECAST DATA BY POSITION *******************************************
+const URL_FORECAST='http://api.openweathermap.org/data/2.5/forecast?q='
+export const getForecastData=async(query,setForecastData)=>{
+    try {
+        let response=await fetch(URL_FORECAST+query+APP_ID)
+        if(response.ok){
+            let data=await response.json()
+            setForecastData(data)
+            console.log('FORECAST DATA FROM FETCH',data)
+        }else{
+            console.log('SOMETHING WENT WRONG')
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
