e33e89dfababe5fcd068e2ebcc2e629db7fd1d21
marcosaccarola <marco.sacca.ads@gmail.com>
2021-10-29 16:49:54 +0200

    weird but true

--- src/components/01-homepage.jsx
+++ src/components/01-homepage.jsx
@@ -68,6 +68,9 @@ const HomePage=()=>{
                 <Col>
                     {actualData&&(
                     <ListGroup>
+                        <ListGroup.Item className='bg-success text-dark'>
+                            AT THIS EXACT MOMENT
+                        </ListGroup.Item>
                         <ListGroup.Item className='bg-dark'>
                             place: {actualData.name}, {actualData.sys.country}
                         </ListGroup.Item>
@@ -85,6 +88,21 @@ const HomePage=()=>{
                         </ListGroup.Item>
                     </ListGroup>
                     )}
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    {forecastData&&forecastData.list.map((f)=>(
+                        <ListGroup key={f.dt}>
+                            <ListGroup.Item className='bg-warning text-dark'>
+                                FORECAST <small>{f.dt_txt}</small>
+                            </ListGroup.Item>
+                            <ListGroup.Item className='bg-dark'>
+                                 {parseInt(f.main.temp-273.15)}°C | {f.weather[0].main} 
+                                <img src={`http://openweathermap.org/img/wn/${f.weather[0].icon}@2x.png`} alt={'an icon'} />
+                            </ListGroup.Item>
+                        </ListGroup>
+                    ))}
                 </Col>
             </Row>
         </Container>
