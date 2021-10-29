1522a17028348fd8dc3a81eb63333ddca1665291
marcosaccarola <marco.sacca.ads@gmail.com>
2021-10-29 16:22:44 +0200

    forecast fetch

--- src/components/01-homepage.jsx
+++ src/components/01-homepage.jsx
@@ -1,35 +1,49 @@
 import { useState } from "react"
 import { Col, Container, Form, FormControl, ListGroup, Row } from "react-bootstrap"
 
-
-
 const HomePage=()=>{
     const[query,setQuery]=useState('')
-    const[positionData,setPositionData]=useState()
-
-    //************************************** POSITION DATA ***************************************
+    const[actualData,setActualData]=useState()
+    const[forecastData,setForecastData]=useState()
+    const APP_ID=process.env.REACT_APP_APP_ID
     const handleChange=(e)=>{setQuery(e.target.value)}
     const handleSubmit=(e)=>{
         e.preventDefault()
-        getPositionData(query)
+        getActualData(query)
+        getForecastData(query)
     }
-    const APP_ID=process.env.REACT_APP_APP_ID
+    //************************************** ACTUAL DATA BY POSITION ***************************************
     const URL='http://api.openweathermap.org/data/2.5/weather?q='
-    const getPositionData=async(query)=>{
+    const getActualData=async(query)=>{
         try {
             let response=await fetch(URL+query+APP_ID)
             if(response.ok){
                 let data=await response.json()
-                setPositionData(data)
+                setActualData(data)
             }else{
                 console.log('SOMETHING WENT WRONG')
             }
         } catch (error) {
             console.log(error)
         }
-        console.log('POSITION DATA FROM FETCH',positionData)
+        console.log('ACTUAL DATA FROM FETCH',actualData)
     }
-    //****************************************************************************
+    //********************************* FORECAST DATA BY POSITION *******************************************
+    const URL_FORECAST='http://api.openweathermap.org/data/2.5/forecast?q='
+    const getForecastData=async(query)=>{
+        try {
+            let response=await fetch(URL_FORECAST+query+APP_ID)
+            if(response.ok){
+                let data=await response.json()
+                setForecastData(data)
+            }else{
+                console.log('SOMETHING WENT WRONG')
+            }
+        } catch (error) {
+            console.log(error)
+        }
+        console.log('FORECAST DATA FROM FETCH',forecastData)
+    }
 
     return(
         <Container fluid className="px-4">
@@ -52,19 +66,22 @@ const HomePage=()=>{
             </Row>
             <Row>
                 <Col>
-                    {positionData&&(
+                    {actualData&&(
                     <ListGroup>
                         <ListGroup.Item className='bg-dark'>
-                            temperature: {parseInt(positionData.main.temp-273.15)}°C | 
-                            clouds: {positionData.clouds.all}% 
+                            place: {actualData.name}, {actualData.sys.country}
                         </ListGroup.Item>
                         <ListGroup.Item className='bg-dark'>
-                            max today: {parseInt(positionData.main.temp_max-273.15)}°C | 
-                            min today: {parseInt(positionData.main.temp_min-273.15)}°C
+                            temperature: {parseInt(actualData.main.temp-273.15)}°C | 
+                            clouds: {actualData.clouds.all}% 
                         </ListGroup.Item>
                         <ListGroup.Item className='bg-dark'>
-                            humidity: {positionData.main.humidity}% | 
-                            pressure: {positionData.main.pressure} mbar
+                            max today: {parseInt(actualData.main.temp_max-273.15)}°C | 
+                            min today: {parseInt(actualData.main.temp_min-273.15)}°C
+                        </ListGroup.Item>
+                        <ListGroup.Item className='bg-dark'>
+                            humidity: {actualData.main.humidity}% | 
+                            pressure: {actualData.main.pressure} mbar
                         </ListGroup.Item>
                     </ListGroup>
                     )}
